f2dbf1df216b4385386786d24d0d970b5a56a2a1
Armando63 <loelkk33@gmail.com>
2026-05-21 15:06:26 -0600

    Se realizo el punto 4 de la actividad

--- c/client.c
+++ c/client.c
@@ -30,6 +30,7 @@ int main()
     struct sockaddr_in serverAddr;  //Direccion del servidor
     char user[50];  //Almacenara el nombre del usuario
     char message[BUFFER];   //Tamaño maximo de los mensaje
+	
 
     socketClient = socket(AF_INET, SOCK_STREAM, 0); //Creacion del socket - IPV4, Conexion estable, Protocolo
 
@@ -49,13 +50,40 @@ int main()
         exit(1);
     }
 
-    printf("Username: ");   //Peticion de nombre de usuario
-    fgets(user, sizeof(user), stdin);   //Lectura del nombre
-    send(socketClient, user, strlen(user), 0);  //Envio de datos al servidor - Socket propio. nombre de usuaion longitud, bandera
+	while(1) //Ciclo de verificacion de nombre de usuario
+	{
+		printf("Username: ");   //Peticion de nombre de usuario
+		fgets(user, sizeof(user), stdin);   //Lectura del nombre
+		send(socketClient, user, strlen(user), 0);  //Envio de datos al servidor - Socket propio. nombre de usuaion longitud, bandera
+		
+		int bytes = recv(socketClient, message, BUFFER, 0); //Respuesta del servidor
+		
+		if(bytes <= 0) //Si se desconecto del servidor...
+		{
+			printf("Disconnected from server\n");
+			close(socketClient);
+			return 1;
+		}
+		
+		message[bytes] = '\0'; //Terminador de cadena
+		
 
-    pthread_t thread;   //Hilo
-    pthread_create(&thread, NULL, receiveMessage, &socketClient);   //Creacion del hilo que recibira mensajes - Hilo,NULL, programa, destino 
-
+		if(strstr(message, "Error!") != NULL) // Si el nombre esta repetido
+		{
+			printf("%s\n", message);
+		}
+		else
+		{
+			printf("%s\n", message); //Si no lo esta, se sale del ciclo
+			break;
+		}
+		
+	}
+	
+	pthread_t thread;   //Hilo
+	pthread_create(&thread, NULL, receiveMessage, &socketClient);   //Creacion del hilo que recibira mensajes - Hilo,NULL, programa, destino 
+	
+	
     while(1)    //Espera
     {
         fgets(message, sizeof(message), stdin); //Captura mensaje a enviar
